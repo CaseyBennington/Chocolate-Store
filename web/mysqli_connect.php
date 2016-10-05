5860444af1da2bb29ffd2537146fda8b2eba7ee6
Casey Bennington <cbenning@vt.edu>
2016-10-05 04:02:20 -0700

    heroku adjustments

--- web/mysqli_connect.php
+++ web/mysqli_connect.php
@@ -1,13 +1,20 @@
 <?php # mysqli_connect.php
-// This file contains the database access information. 
-// This file also establishes a connection to MySQL 
+// This file contains the database access information.
+// This file also establishes a connection to MySQL
 // and selects the database.
 
 // Set the database access information as constants:
-DEFINE ('DB_USER', 'itp225');
-DEFINE ('DB_PASSWORD', 'itp225');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'cbennington225proj');
+// DEFINE ('DB_USER', 'itp225');
+// DEFINE ('DB_PASSWORD', 'itp225');
+// DEFINE ('DB_HOST', 'localhost');
+// DEFINE ('DB_NAME', 'cbennington225proj');
+
+DEFINE ('DB_USER', 'b17eb82e7165b2');
+DEFINE ('DB_PASSWORD', '639afb32');
+DEFINE ('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
+DEFINE ('DB_NAME', 'heroku_375ba6db2105b5f');
+
+// mysql://b17eb82e7165b2:639afb32@us-cdbr-iron-east-04.cleardb.net/heroku_375ba6db2105b5f?reconnect=true
 
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
